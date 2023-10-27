915ac8097e41ced30c4e1337033ac051c9aa929c
Nikhil M <149003438+nikhilmblore@users.noreply.github.com>
2023-10-28 01:07:30 +0530

    Create P50D_03_casting.py
    day 3

---
+++ Code/P50D_03_casting.py
@@ -0,0 +1,14 @@
+# Typecasting
+
+x = int(3.4) #
+print(x)
+x =str("45") #
+print(x)
+x = float(3) #3.0
+print(x)
+x = float("3.4") #3.4
+print(x)
+x = str(4) #”4”
+print(x)
+x = str(4.5) #”4.5”
+print(x)
