6b878ef59ec12d57d722bd02bd2c9ac201598497
Nikhil M <149003438+nikhilmblore@users.noreply.github.com>
2023-10-28 19:31:16 +0530

    Create P50D_04_string_methods.py

---
+++ Code/P50D_04_string_methods.py
@@ -0,0 +1,31 @@
+# String methods
+# --------------------------------------------
+# strip()
+# replace()
+# Split()
+# format()
+# --------------------------------------------
+
+# strip()
+tempString = "   this is strip method     "
+print(tempString.strip())
+print(tempString) 
+
+# replace()
+tempString = "This is replace method"
+print(tempString.replace("me","aa"))
+print(tempString)
+
+# split()
+tempString = "This is split method"
+print(tempString.split())
+print(tempString.split("i"))
+print(tempString)
+
+# format
+tempString = "My name is {} {}"
+print(tempString.format("nikhil", "raju"))
+print(tempString) #prints unformatted string
+
+
+
